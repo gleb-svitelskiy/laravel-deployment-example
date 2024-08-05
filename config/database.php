d82d928f6844ab067e45e021456034cf5fbe91cc
Gleb Svitelskiy <gleb.svitelskiy@gmail.com>
2024-08-05 16:14:15 +0300

    add Docker

--- config/database.php
+++ config/database.php
@@ -148,6 +148,7 @@ return [
         'options' => [
             'cluster' => env('REDIS_CLUSTER', 'redis'),
             'prefix' => env('REDIS_PREFIX', Str::slug(env('APP_NAME', 'laravel'), '_').'_database_'),
+            'serializer' => Redis::SERIALIZER_IGBINARY,
         ],
 
         'default' => [
--- routes/web.php
+++ routes/web.php
@@ -7,17 +7,37 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/log', function () {
+    logger('Debug message');
+});
+
 Route::get('/phpinfo', function () {
     return phpinfo();
 });
 
 Route::get('/request', function (Request $request) {
-    dump($request->fullUrl());
-    dump($request->host());
-    dump($request->httpHost());
-    dump($request->schemeAndHttpHost());
-    dump($request->header());
+    dump('fullUrl', $request->fullUrl());
+    dump('host', $request->host());
+    dump('httpHost', $request->httpHost());
+    dump('schemeAndHttpHost', $request->schemeAndHttpHost());
+    dump('header', $request->header());
     dump('ip', $request->ip());
     dump('ips', $request->ips());
     dump($request);
+});
+
+Route::get('/cache-set', function () {
+    cache(['key' => 'value']);
+});
+Route::get('/cache-get', function () {
+    return cache('key');
+});
+Route::get('/session-set', function () {
+    session(['key' => time()]);
+});
+Route::get('/session-get', function () {
+    return session('key');
+});
+Route::get('/hostname', function () {
+    return gethostname();
 });
